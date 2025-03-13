069567a311b066e10015124baae2f9011730c39e
arrowreal <gunlubaki7@gmail.com>
2025-03-13 05:02:21 +0300

    Add project files.

---
+++ Controllers/AnasayfaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace yemektarifi.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        public IActionResult Anasayfa()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/YemekController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace yemektarifi.Controllers
+{
+    public class YemekController : Controller
+    {
+        public IActionResult Manti()
+        {
+            return View();
+        }
+
+        public IActionResult Pilav()
+        {
+            return View();
+        }
+
+        public IActionResult Makarna()
+        {
+            return View();
+        }
+
+        public IActionResult Kereviz()
+        {
+            return View();
+        }
+
+        public IActionResult Yapraksarma()
+        {
+            return View();
+        }
+
+        public IActionResult Karniyarik()
+        {
+            return View();
+        }
+    }
+}
